59c6d35cab530572784ffbaef0c21191f5665a6b
Andrey Shevandrin <shevandrin@gmail.com>
2025-06-29 15:35:21 +0200

    feat: Add new detector function find_elements_by_computed_style

--- botscout/__init__.py
+++ botscout/__init__.py
@@ -2,5 +2,6 @@ __version__ = "0.1.0"
 
 
 from .launcher import launch_page, check_ip
+from ._detector_utils import find_elements_by_computed_style
 
 print(f"BotScout package initialized (version {__version__})")
