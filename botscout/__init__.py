40c397e245c61af59c26b46fc7289e3a3bd2abea
Andrey Shevandrin <shevandrin@gmail.com>
2025-06-29 21:44:30 +0200

    refactor: Change function name to _find_elements_by_computed_style as util

--- botscout/__init__.py
+++ botscout/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.1.0"
 
 
 from .launcher import launch_page, check_ip
-from ._detector_utils import find_elements_by_computed_style
+from ._detector_utils import _find_elements_by_computed_style, _get_html_from_element
 from ._launcher_utils import _handle_cookie_consent
 
 print(f"BotScout package initialized (version {__version__})")
--- botscout/_detector_utils.py
+++ botscout/_detector_utils.py
@@ -1,7 +1,10 @@
+from bs4 import BeautifulSoup
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
 
 
-def find_elements_by_computed_style(driver: WebDriver):
+def _find_elements_by_computed_style(driver: WebDriver):
     """
     Finds all elements that match specific computed style properties
     common to chatbot widgets (e.g., fixed position, high z-index).
@@ -52,3 +55,42 @@ def find_elements_by_computed_style(driver: WebDriver):
     except Exception as e:
         print(f"Error executing computed style search script: {e}")
         return []
+
+
+def _get_html_from_element(element: WebElement, driver: WebDriver):
+    """
+    Provides with HTML of an element, handling iframes.
+    Args:
+        element:
+        driver:
+
+    Returns:
+        The HTML content as a string.
+    """
+
+    wrapper_html = element.get_attribute("outerHTML")
+    soup = BeautifulSoup(wrapper_html, features="html.parser")
+
+    iframe_tag = soup.find('iframe')
+    if not iframe_tag:
+        return wrapper_html
+
+    try:
+        live_iframe = element.find_element(By.TAG_NAME, 'iframe')
+        driver.switch_to.frame(live_iframe)
+        iframe_html = driver.find_element(By.TAG_NAME, "html").get_attribute('outerHTML')
+        driver.switch_to.default_content()
+        print("  - Successfully extracted iframe HTML and switched back.")
+    except Exception as e:
+        print(f"  - Error switching to iframe: {e}")
+        driver.switch_to.default_content()
+        iframe_html = "<!-- Could not retrieve iframe content -->"
+
+    iframe_soup = BeautifulSoup(iframe_html, features="html.parser")
+    iframe_tag.clear()
+    iframe_tag.append(iframe_soup)
+    # print("----")
+    # print(soup.text)
+    # print("----")
+
+    return soup.prettify()
